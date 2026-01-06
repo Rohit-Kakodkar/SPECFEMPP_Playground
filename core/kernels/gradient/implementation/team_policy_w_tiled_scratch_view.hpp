5bf8432f97b2a6e64e90506d52b21b4523dfbafd
Rohit Kakodkar <kakodkar.rohit@gmail.com>
2026-01-06 15:55:08 -0500

    Updated tiled implementation to use fewer registers

--- core/kernels/gradient/implementation/team_policy_w_tiled_scratch_view.hpp
+++ core/kernels/gradient/implementation/team_policy_w_tiled_scratch_view.hpp
@@ -25,7 +25,12 @@ public:
 
     ReturnType operator()() const {
         // Team policy with tiled scratch memory implementation
-        constexpr static size_t tile_size[] = {32, 4, 4};  // {elements, z, x}
+#if defined(KOKKOS_ENABLE_CUDA)
+        constexpr static size_t tile_size[] = {32, 8, 8};  // {elements, z, x}
+#else
+        constexpr static size_t tile_size[] = {1, 8, 8};  // {elements, z, x}
+#endif
+        constexpr static size_t tile_size_e = tile_size[0];
         constexpr static size_t tile_size_z = tile_size[1];
         constexpr static size_t tile_size_x = tile_size[2];
         const size_t nteams =
@@ -36,22 +41,35 @@ public:
         using ScratchSpaceType = Kokkos::View<T[tile_size[0]][tile_size[1]][tile_size[2]][1],
                                               typename execution_space::scratch_memory_space,
                                               Kokkos::MemoryTraits<Kokkos::Unmanaged>>;
-        const size_t scratch_size = 2 * ScratchSpaceType::shmem_size();  // For storing field tiles
+        using LagrangeViewType = Kokkos::View<T[tile_size[2]][tile_size[1]],
+                                              typename execution_space::scratch_memory_space,
+                                              Kokkos::MemoryTraits<Kokkos::Unmanaged>>;
+        const size_t scratch_size =
+            2 * ScratchSpaceType::shmem_size() +
+            2 * LagrangeViewType::shmem_size();  // For field slices & lprime
+
+#if defined(KOKKOS_ENABLE_CUDA)
+        const auto policy = Kokkos::TeamPolicy<>(nteams, Kokkos::AUTO, Kokkos::AUTO)
+                                .set_scratch_size(0, Kokkos::PerTeam(scratch_size));
+#else
+        const auto policy = Kokkos::TeamPolicy<>(nteams, Kokkos::AUTO, Kokkos::AUTO)
+                                .set_scratch_size(0, Kokkos::PerTeam(scratch_size));
+#endif
 
         Kokkos::parallel_for(
-            "GradientComputationTeamPolicyWTiledScratch",
-            Kokkos::TeamPolicy<>(nteams, Kokkos::AUTO, Kokkos::AUTO)
-                .set_scratch_size(0, Kokkos::PerTeam(scratch_size)),
+            "GradientComputationTeamPolicyWTiledScratch", policy,
             KOKKOS_CLASS_LAMBDA(const Kokkos::TeamPolicy<>::member_type& team) {
                 const size_t team_id = team.league_rank();
-                const size_t start_e = team_id * tile_size[0];
-                const size_t end_e = (start_e + tile_size[0] < this->n_elements_)
-                                         ? (start_e + tile_size[0])
+                const size_t start_e = team_id * tile_size_e;
+                const size_t end_e = (start_e + tile_size_e < this->n_elements_)
+                                         ? (start_e + tile_size_e)
                                          : this->n_elements_;
                 const size_t local_chunk_size = end_e - start_e;
 
                 ScratchSpaceType field_x_scratch(team.team_scratch(0));
                 ScratchSpaceType field_z_scratch(team.team_scratch(0));
+                LagrangeViewType xi_(team.team_scratch(0));
+                LagrangeViewType gamma_(team.team_scratch(0));
 
                 for (size_t tile_k = 0; tile_k < this->nx_; tile_k += tile_size_x) {
                     const size_t tile_k_start = tile_k;
@@ -65,19 +83,22 @@ public:
                                                        : this->nz_;
 
                         Kokkos::parallel_for(
-                            Kokkos::TeamThreadRange(team, local_chunk_size *
-                                                              (tile_iz_end - tile_iz_start)),
+                            Kokkos::TeamThreadRange(team, tile_size_e * tile_size_z),
                             [&](const size_t idx) {
-                                const size_t local_e = idx % local_chunk_size;
-                                const size_t iz =
-                                    (idx / local_chunk_size) % (tile_iz_end - tile_iz_start);
-                                const size_t k =
-                                    idx / (local_chunk_size * (tile_iz_end - tile_iz_start));
+                                const size_t local_e = idx % tile_size_e;
+                                const size_t iz = idx / tile_size_e;
+
+                                if (local_e > local_chunk_size - 1 ||
+                                    iz > tile_iz_end - tile_iz_start - 1)
+                                    return;
+
                                 for (size_t c = 0; c < this->ncomponents_; ++c) {
-                                    for (size_t x = 0; x < tile_size_x; ++x) {
-                                        field_x_scratch(local_e, iz, x, c) =
+                                    for (size_t k = 0; k < tile_size_x; ++k) {
+                                        field_x_scratch(local_e, iz, k, c) =
                                             this->field_(start_e + local_e, iz + tile_iz_start,
                                                          k + tile_k_start, c);
+                                        gamma_(iz, k) = this->lprime_.gamma(iz + tile_iz_start,
+                                                                            k + tile_k_start);
                                     }
                                 }
                             });
@@ -89,17 +110,22 @@ public:
                                                            : this->nx_;
 
                             Kokkos::parallel_for(
-                                Kokkos::TeamThreadRange(team, local_chunk_size *
-                                                                  (tile_ix_end - tile_ix_start)),
+                                Kokkos::TeamThreadRange(team, tile_size_e * tile_size_x),
                                 [&](const size_t idx) {
-                                    const size_t local_e = idx % local_chunk_size;
-                                    const size_t ix =
-                                        (idx / local_chunk_size) % (tile_ix_end - tile_ix_start);
+                                    const size_t local_e = idx % tile_size_e;
+                                    const size_t ix = idx / tile_size_e;
+
+                                    if (local_e > local_chunk_size - 1 ||
+                                        ix > tile_ix_end - tile_ix_start - 1)
+                                        return;
+
                                     for (size_t c = 0; c < this->ncomponents_; ++c) {
                                         for (size_t k = 0; k < tile_size_z; ++k) {
                                             field_z_scratch(local_e, k, ix, c) =
                                                 this->field_(start_e + local_e, k + tile_iz_start,
                                                              ix + tile_ix_start, c);
+                                            xi_(ix, k) = this->lprime_.xi(ix + tile_ix_start,
+                                                                          k + tile_k_start);
                                         }
                                     }
                                 });
@@ -107,29 +133,30 @@ public:
                             team.team_barrier();
 
                             Kokkos::parallel_for(
-                                Kokkos::TeamThreadRange(team, local_chunk_size *
-                                                                  (tile_iz_end - tile_iz_start) *
-                                                                  (tile_ix_end - tile_ix_start)),
+                                Kokkos::TeamThreadRange(team,
+                                                        tile_size_e * tile_size_z * tile_size_x),
                                 [&](const size_t idx) {
-                                    const size_t local_e = idx % local_chunk_size;
-                                    const size_t iz =
-                                        (idx / local_chunk_size) % (tile_iz_end - tile_iz_start);
-                                    const size_t ix =
-                                        idx / (local_chunk_size * (tile_iz_end - tile_iz_start));
+                                    const size_t local_e = idx % tile_size_e;
+                                    const size_t iz = (idx / tile_size_e) % tile_size_z;
+                                    const size_t ix = idx / (tile_size_e * tile_size_z);
+
+                                    if (local_e > local_chunk_size - 1 ||
+                                        iz > tile_iz_end - tile_iz_start - 1 ||
+                                        ix > tile_ix_end - tile_ix_start - 1)
+                                        return;
+
                                     T du_dxi[this->ncomponents_] = {static_cast<T>(0)};
                                     T du_dgamma[this->ncomponents_] = {static_cast<T>(0)};
                                     for (size_t k = 0; k < tile_size_x; ++k) {
                                         for (size_t c = 0; c < this->ncomponents_; ++c) {
-                                            du_dxi[c] += this->lprime_.xi(ix + tile_ix_start,
-                                                                          k + tile_k_start) *
-                                                         field_x_scratch(local_e, iz, k, c);
+                                            du_dxi[c] +=
+                                                xi_(ix, k) * field_x_scratch(local_e, iz, k, c);
                                         }
                                     }
                                     for (size_t k = 0; k < tile_size_z; ++k) {
                                         for (size_t c = 0; c < this->ncomponents_; ++c) {
-                                            du_dgamma[c] += this->lprime_.gamma(iz + tile_iz_start,
-                                                                                k + tile_k_start) *
-                                                            field_z_scratch(local_e, k, ix, c);
+                                            du_dgamma[c] +=
+                                                gamma_(iz, k) * field_z_scratch(local_e, k, ix, c);
                                         }
                                     }
 
